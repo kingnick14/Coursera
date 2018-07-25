f7f3788e44e4d658018a03f8fc8272efafaab0cb
King Nick <nar@marketlogicsoftware.com>
2018-07-25 18:53:07 +0200

    asd

--- ch12ex1test.py
+++ ch12ex1test.py
@@ -1,12 +1,16 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('hi')
 mysock.connect(('www.w3.org', 80))
-cmd = 'GET https://www.w3.org/TR/PNG/iso_8859-1.txt HTTP/1.0\r\n\r\n'.encode()
+print('poop')
+cmd = 'GET https://www.w3.org/TR/PNG/iso_8859-1.txt HTTP/1.0\n\n'.encode()
 mysock.send(cmd)
-
+print('fart')
 while True:
-    data = mysock.recv(20)
+    data = mysock.recv(4096)
+    print('guido')
+    print(data)
     if (len(data) < 1):
         break
     print(data.decode(),end='')
