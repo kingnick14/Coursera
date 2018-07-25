2c117b9e5c8f6d49f838a37c6f36aa8adf4c36d1
kingnick14 <40497055+kingnick14@users.noreply.github.com>
2018-07-25 21:31:10 +0200

    sdsd

--- ch12ex2.py
+++ ch12ex2.py
@@ -7,16 +7,16 @@ try:
 
     mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mysock.connect((inpSocket[2], 80))
-    cmd = ('GET ' + inp + ' HTTP/1.0\r\n\r\n').encode()
+    cmd = str('GET ' + inp + ' HTTP/1.0\r\n\r\n').encode()
     mysock.send(cmd)
-
+    dl = 0
     while True:
-        data = mysock.recv(512)
-        if len(data) < 1:
-            break
-        print(data.decode(),end='')
-
+        data = mysock.recv(300)
+        dl = dl + len(data)
+        if len(data) < 1: break
+        elif dl <= 3000: print(data.decode(),end='')
+    print('\n\nnico:',dl)
     mysock.close()
 
-except:
+except socket.gaierror:
     print('Oops!  That was no valid URL.  Try again...')
